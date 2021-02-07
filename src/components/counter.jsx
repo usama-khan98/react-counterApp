ce1e32153a74846ecec28ef2b32df5b99ab1bd0b
usama khan <usama.alikhan98@gmail.com>
2021-02-07 16:18:16 +0000

    raising and handlind delete event

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,17 +2,11 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
 	state = {
-		count: 0
-		// tags: [ 'tag1', 'tag2', 'tag3' ]
+		value: this.props.value
 	};
 
-	// constructor() {
-	// 	super();
-	// 	this.handleIncrement = this.handleIncrement.bind(this);
-	// }
-
 	handleIncrement = () => {
-		this.setState({ count: this.state.count + 1 });
+		this.setState({ value: this.state.value + 1 });
 	};
 
 	render() {
@@ -25,24 +19,27 @@ class Counter extends Component {
 					onClick={this.handleIncrement}
 					className="btn btn-secondary btn-sm"
 				>
-					+
+					Increment
 				</button>
-				{/* <ul>
-					{this.state.tags.map((tag) => <li key={tag}> {tag} </li>)}
-				</ul> */}
+				<button
+					onClick={() => this.props.onDelete(this.props.id)}
+					className="btn btn-danger btn-sm m-2"
+				>
+					Delete
+				</button>
 			</div>
 		);
 	}
 
 	getBadgeClasses() {
 		let classes = 'badge m-2 badge-';
-		classes += this.state.count === 0 ? 'warning' : 'primary';
+		classes += this.state.value === 0 ? 'warning' : 'primary';
 		return classes;
 	}
 
 	formatCount() {
-		const { count } = this.state;
-		return count === 0 ? 'Zero' : count;
+		const { value } = this.state;
+		return value === 0 ? 'Zero' : value;
 	}
 }
 
